6531b98ec1aca5646d1f324ab87aa4058910d8e3
Fernando <aa@AAs-MacBook-Air.local>
2018-09-03 14:09:55 -0700

    Refactor JS using JQuery.

--- website/scripts.js
+++ website/scripts.js
@@ -1,17 +1,14 @@
 // Filters links list in response to entering of search query character.
 function searchLinks() {
-    var searchInput = document.getElementById('searchBar');
-    var filter = searchInput.value.toLowerCase();
-
-    // Loop through all list items, and hide those who don't match the search query.
-    var links = $("#myLinks li");
+    // Loop through all list items, and hide those who don't match the search filter.
+    const filter = $("#searchBar")[0].value.toLowerCase();
+    const links = $("#myLinks li");
     for (var i=0; i < links.length; i++) {
-        var as = links[i].getElementsByTagName("a");
-        var linkTag = as[0];
-        if (linkTag.innerHTML.toLowerCase().indexOf(filter) > -1) {
-            links[i].style.display = "";
+        const linkName = $(links[i]).find("a")[0];
+        if ($(linkName).html().toLowerCase().indexOf(filter) == 0) {
+            $(links[i]).show();
         } else {
-            links[i].style.display = "none";
+            $(links[i]).hide();
         }
     }
 }
@@ -20,8 +17,8 @@ function searchLinks() {
 function showLinks() {
     $.getJSON("https://cloudlinks.nfshost.com/links.php", function(data){
       for(i in data.links) {
-        var url = data.links[i].url;
-	var favicon_url = url + "/favicon.ico";
+        const url = data.links[i].url;
+	const favicon_url = url + "/favicon.ico";
         $("#myLinks").append("<li>" +
           "<img src=\"link_type_out.png\">&nbsp;" + 
           "<img src=\"" + favicon_url + "\" width=\"16\">&nbsp;" +
